c8fa20ae3bdb3e2afdac4186052ccf6dc511ab05
Thomas Roeder <a@a.com>
2016-04-13 20:55:58 +0200

    highcharts added

--- RailsHomeAutomation/app/controllers/sensors_controller.rb
+++ RailsHomeAutomation/app/controllers/sensors_controller.rb
@@ -1,2 +1,25 @@
 class SensorsController < ApplicationController
+
+  # Iterate through all elements in db an convert to string in format of javascript array [ [time1, value1], [time2, value2], time3, value3] ]
+  # this format is understood by highcharts.
+  def self.get_temperature()
+    s = "[ "
+    Sensor.all.each do |w|
+      s = s + " [" + w.timestamp.to_datetime.strftime('%Q').to_s +  ", " + w.temperature.to_s + "] ,"
+    end
+    s = s + " ]"
+    return s
+  end
+
+  # Iterate through all elements in db an convert to string in format of javascript array [ [time1, value1], [time2, value2], time3, value3] ]
+  # this format is understood by highcharts.
+  def self.get_pressure()
+    s = "[ "
+    Sensor.all.each do |w|
+      s = s + " [" + w.timestamp.to_datetime.strftime('%Q').to_s +  ", " + w.pressure.to_s + "] ,"
+    end
+    s = s + " ]"
+    return s
+  end
+
 end
