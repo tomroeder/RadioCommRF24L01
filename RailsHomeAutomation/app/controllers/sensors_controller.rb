c670d8cfbbf9820d15f4ecf92c0e386a66cbf2ff
Thomas Roeder <a@a.com>
2016-04-17 21:14:05 +0200

    Improved view

--- RailsHomeAutomation/app/controllers/sensors_controller.rb
+++ RailsHomeAutomation/app/controllers/sensors_controller.rb
@@ -27,6 +27,14 @@ class SensorsController < ApplicationController
     return Sensor.last.pressure.round(1).to_s
   end
 
+  # return the pressure value in the middle between min and max. 
+  def self.get_minmax_middle_pressure()
+    minPressure = Sensor.minimum("pressure")
+    maxPressure = Sensor.maximum("pressure")
+    d = maxPressure-minPressure
+    return minPressure +d/2
+  end
+
   # Iterate through all elements in db an convert to string in format of javascript array [ [time1, value1], [time2, value2], time3, value3] ]
   # this format is understood by highcharts.
   # round(1) - round to precision 1 after .
